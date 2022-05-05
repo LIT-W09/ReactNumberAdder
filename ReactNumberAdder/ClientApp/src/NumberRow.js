28143ee77e17fb169110ad8845f18fa68081774d
Avrumi Friedman <a.friedman07@gmail.com>
2022-05-04 21:45:46 -0400

    Add project files.

---
+++ ReactNumberAdder/ClientApp/src/NumberRow.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// class NumberRow extends React.Component {
+//     render() {
+//         const { number, isAdd, onAddClick, onRemoveClick, locked } = this.props;
+//         return (
+//             <tr>
+//                 <td>{number}</td>
+//                 <td>
+//                     {isAdd ? <button className="btn btn-primary" onClick={onAddClick}>Add to Selected</button>
+//                         : <button disabled={locked} className="btn btn-danger" onClick={onRemoveClick}>Remove from Selected</button>
+//                     }
+//                 </td>
+//             </tr>
+//         )
+//     }
+// }
+
+function NumberRow({ number, isAdd, onAddClick, onRemoveClick, locked }) {
+    return (
+        <tr>
+            <td>{number}</td>
+            <td>
+                {isAdd ? <button className="btn btn-primary" onClick={onAddClick}>Add to Selected</button>
+                    : <button disabled={locked} className="btn btn-danger" onClick={onRemoveClick}>Remove from Selected</button>
+                }
+            </td>
+        </tr>
+    )
+}
+
+
+export default NumberRow;
